655dc9aa4375fb0acfcf00c263e58af53630624a
Fernando Martinez <165216584+Fercho36@users.noreply.github.com>
2026-05-23 14:37:50 -0600

    Delete update_db.py

--- update_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-
-with app.app_context():
-    # Esto creará las tablas RemoteCareLink y RemoteMeasurement si no existen.
-    db.create_all()
-    print("Base de datos actualizada con los nuevos modelos.")
